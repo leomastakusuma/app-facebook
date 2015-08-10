e4e11e98196c30d5802c314aeec3e7e0b1f7d698
leomastakusuma <leomastakusuma@gmail.com>
2015-08-10 21:17:49 +0700

    Update BB Piutang

--- fb-callback.php
+++ fb-callback.php
@@ -75,6 +75,7 @@ $_SESSION['fb_access_token'] = (string) $accessToken;
 try {
   // Returns a `Facebook\FacebookResponse` object
   $response = $fb->get('/me', $accessToken);
+  pr($response);
 } catch(Facebook\Exceptions\FacebookResponseException $e) {
   echo 'Graph returned an error: ' . $e->getMessage();
   exit;
