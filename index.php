1db997f22cdf27479ea77e0cb10d34ba13d35fd8
leomastakusuma <leomastakusuma@gmail.com>
2015-08-10 15:25:33 +0700

    login

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
 <?php
 
+require 'src/Facebook/autoload.php';
+session_start();
+
+$fb = new Facebook\Facebook([
+  'app_id' => '1673188132913490',
+  'app_secret' => '6e435fefb77a7972eeceb10dd81df0cb',
+  'default_graph_version' => 'v2.4',
+  ]);
+
+$helper = $fb->getRedirectLoginHelper();
+$permissions = ['email']; // Optional permissions
+$loginUrl = $helper->getLoginUrl('http://mdpu-finance.com/app-facebook/fb-callback.php', $permissions);
+
+echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
